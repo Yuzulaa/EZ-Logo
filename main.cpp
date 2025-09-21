0c68943dfcc83fb3e982d51d10c647d97fad1bbb
Yuzulaa <mikomiru8@gmail.com>
2025-09-22 07:51:59 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    // Top border of the square
+    std::cout << "+-----------+" << std::endl;
+    
+    
+    std::cout << "| **** **** |" << std::endl;
+    std::cout << "| *      *  |" << std::endl;
+    std::cout << "| ****  *   |" << std::endl;
+    std::cout << "| *    *    |" << std::endl;
+    std::cout << "| **** **** |" << std::endl;
+    
+    
+    std::cout << "+-----------+" << std::endl;
+    
+    return 0;
+}
